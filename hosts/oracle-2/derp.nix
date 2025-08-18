d9f181cf678ce64adf5c9f9cc177f3ed0fc28009
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-18 16:23:45 +0300

    feat(oracle-2/tailscale): init DERP

---
+++ hosts/oracle-2/derp.nix
@@ -0,0 +1,50 @@
+{ config, lib, ... }:
+let
+  inherit (lib)
+    enabled
+    ;
+
+  domain = "derp.r3dlust.com";
+in
+{
+  networking.certificates."${domain}" = {
+    enable = true;
+    group = "main-certs"; # no problem in doing this as main-certs already only has "caddy"
+    users = [
+      "caddy"
+    ];
+
+    postRun = ''
+      systemctl reload-or-restart caddy.service
+    '';
+  };
+
+  services.tailscale.derper = enabled {
+    port = 48822;
+    domain = "derp.r3dlust.com";
+    openFirewall = false;
+    verifyClients = false; # TODO check if this works
+    configureNginx = false; # we use caddy in this house
+  };
+
+  services.caddy.virtualHosts."${domain}" = {
+    extraConfig = ''
+      tls ${config.networking.certificates."${domain}".paths.fullchain} ${
+        config.networking.certificates."${domain}".paths.key
+      }
+
+      reverse_proxy localhost:${toString config.services.tailscale.derper.port} {
+        flush_interval -1
+
+        transport http {
+          read_timeout 3600s
+          write_timeout 3600s
+        }
+      }
+    '';
+  };
+
+  networking.firewall.allowPing = true;
+
+  networking.firewall.allowedUDPPorts = [ config.services.tailscale.derper.stunPort ];
+}
