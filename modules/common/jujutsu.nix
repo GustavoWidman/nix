a9f8262c7328c02c2b117c7a151c16456e9c6016
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-22 11:42:49 -0300

    feat(jj): start using jujutsu VCS

---
+++ modules/common/jujutsu.nix
@@ -0,0 +1,87 @@
+{
+  lib,
+  ...
+}:
+let
+  inherit (lib) enabled;
+in
+{
+  home-manager.sharedModules = [
+    (
+      homeArgs:
+      let
+        config' = homeArgs.config;
+      in
+      {
+        programs.jujutsu = enabled {
+          settings = {
+            user.name = config'.programs.git.userName;
+            user.email = config'.programs.git.userEmail;
+
+            aliases.".." = [
+              "edit"
+              "@-"
+            ];
+            aliases.",," = [
+              "edit"
+              "@+"
+            ];
+
+            aliases.fetch = [
+              "git"
+              "fetch"
+            ];
+            aliases.f = [
+              "git"
+              "fetch"
+            ];
+
+            aliases.push = [
+              "git"
+              "push"
+            ];
+            aliases.p = [
+              "git"
+              "push"
+            ];
+
+            aliases.clone = [
+              "git"
+              "clone"
+              "--colocate"
+            ];
+            aliases.cl = [
+              "git"
+              "clone"
+              "--colocate"
+            ];
+
+            aliases.init = [
+              "git"
+              "init"
+              "--colocate"
+            ];
+            aliases.i = [
+              "git"
+              "init"
+              "--colocate"
+            ];
+
+            git.auto-local-bookmark = true;
+
+            git.fetch = [
+              "origin"
+              "upstream"
+              "rad"
+            ];
+            git.push = "origin";
+
+            signing.backend = "ssh";
+            signing.behavior = "own";
+            signing.key = "~/.ssh/id_ed25519";
+          };
+        };
+      }
+    )
+  ];
+}
