149258dfa106fd60ce10609aae577f23e3207f44
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-27 10:23:38 -0300

    fix(alttab): adds a custom launchagent
    fixes an annoying visual glitch/feature? where
    macos kept reporting "alttab" is sharing your
    screen (very very annoying at times) :P

--- modules/darwin/login.nix
+++ modules/darwin/login.nix
@@ -1,5 +1,13 @@
 { pkgs, ... }:
-
+let
+  altTabScript = pkgs.writeText "alttab.scpt" ''
+    tell application "Terminal"
+    	  do script "screen -dmS AltTab ${pkgs.alt-tab-macos}/Applications/AltTab.app/Contents/MacOS/AltTab"
+      delay 1.5
+      if it is running then quit
+    end tell
+  '';
+in
 {
   system.defaults.loginwindow = {
     DisableConsoleAccess = true;
@@ -9,5 +17,11 @@
   system.login-items = with pkgs; [
     unnaturalscrollwheels
     stats
+    {
+      package = pkgs.alt-tab-macos;
+      command = "/usr/bin/osascript ${altTabScript}";
+      keepAlive = false;
+    }
   ];
+
 }
