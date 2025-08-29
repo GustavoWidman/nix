bcbe6b381e11ab2b43b1c89936e2cf20ce252865
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-08-29 10:54:21 -0300

    chore(cache): update flake inputs and mess around with binary caches

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://cache.garnix.io/"
+      "https://install.determinate.systems"
+      "https://cache.garnix.io"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
-      "https://install.determinate.systems"
     ];
 
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "determinate-systems.cachix.org-1:8hfO/4KM4BUMONABo3NyuTsIB9YLUo5aIwGPg2A6Zs4="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     ];
 
     experimental-features = [
