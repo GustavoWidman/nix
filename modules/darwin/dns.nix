a2e689e4b9b63b3be73dde49dee5d8bd1cab5e20
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-09-15 21:27:53 -0300

    feat(dns): swap from coredns to dnsproxy

--- modules/darwin/dns.nix
+++ modules/darwin/dns.nix
@@ -2,26 +2,14 @@
   pkgs,
   ...
 }:
-let
-  coredns = pkgs.coredns.override {
-    externalPlugins = [
-      {
-        name = "https";
-        repo = "github.com/v-byte-cpu/coredns-https";
-        version = "v0.1.0";
-      }
-    ];
-    vendorHash = "sha256-wVOl1GyVZ8wU6QRz4Y3YN12EeudAiDwKmtY/LcquSWc=";
-  };
-in
 {
-  launchd.daemons.coredns = {
-    command = "${coredns}/bin/coredns -conf ${../common/dns/Corefile}";
+  launchd.daemons.dnsproxy = {
+    command = "${pkgs.dnsproxy}/bin/dnsproxy --config-path=${../common/dns/config.yaml}";
     serviceConfig = {
       KeepAlive = true;
       RunAtLoad = true;
-      StandardOutPath = "/var/log/coredns.log";
-      StandardErrorPath = "/var/log/coredns.log";
+      StandardOutPath = "/var/log/dnsproxy.log";
+      StandardErrorPath = "/var/log/dnsproxy.log";
     };
   };
 }
--- modules/linux/dns.nix
+++ modules/linux/dns.nix
@@ -1,23 +1,14 @@
 {
-  pkgs,
+  lib,
   ...
 }:
 let
-  coredns = pkgs.coredns.override {
-    externalPlugins = [
-      {
-        name = "https";
-        repo = "github.com/v-byte-cpu/coredns-https";
-        version = "v0.1.0";
-      }
-    ];
-    vendorHash = "sha256-wVOl1GyVZ8wU6QRz4Y3YN12EeudAiDwKmtY/LcquSWc=";
-  };
+  inherit (lib)
+    enabled
+    ;
 in
 {
-  services.coredns = {
-    enable = true;
-    package = coredns;
-    config = builtins.readFile ../common/dns/Corefile;
+  services.dnsproxy = enabled {
+    flags = [ "--config-path=${../common/dns/config.yaml}" ];
   };
 }
