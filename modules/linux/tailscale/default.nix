8eb24b8b265b380c633bcc8d45e1f7750b989b0a
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-03 16:23:45 +0300

    feat(tailscale): add options for exit-node and subnets

--- modules/linux/tailscale/default.nix
+++ modules/linux/tailscale/default.nix
@@ -6,17 +6,48 @@
 }:
 let
   inherit (lib)
-    enabled
+    length
+    mkIf
     ;
 in
 {
-  secrets.tailscale-key.file = ./auth-key.age;
-  services.tailscale = enabled {
-    enable = true;
-    package = pkgs.tailscale;
-    authKeyFile = config.secrets.tailscale-key.path;
-    extraUpFlags = [
-      "--accept-dns=false"
-    ];
+  config = {
+    secrets.tailscale-key.file = ./auth-key.age;
+    services.tailscale = mkIf config.tailscale.enable {
+      enable = true;
+      package = pkgs.tailscale;
+      authKeyFile = config.secrets.tailscale-key.path;
+      extraUpFlags = [
+        "--accept-dns=false"
+      ]
+      ++ (lib.optional config.tailscale.exit-node "--advertise-exit-node=true")
+      ++ (lib.optional (length config.tailscale.advertise-routes > 0) (
+        "--advertise-routes=" + (lib.concatStringsSep "," config.tailscale.advertise-routes)
+      ));
+      extraSetFlags = config.services.tailscale.extraUpFlags;
+      useRoutingFeatures = mkIf (
+        config.tailscale.exit-node || (length config.tailscale.advertise-routes > 0)
+      ) "server";
+    };
+  };
+
+  options.tailscale = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Whether to enable Tailscale on this device.";
+    };
+
+    advertise-routes = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "List of IP routes to advertise to the Tailscale network, e.g. [ \"10.0.0.0/8\" \"192.168.0.0/24\" ].";
+    };
+
+    exit-node = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Whether this device should act as an exit node for the Tailscale network.";
+    };
   };
 }
