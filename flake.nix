2dc2f6a52af82126c489deef7df83129edbc213c
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-11 01:46:56 -0300

    chore(nix): remove unused binary caches

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   nixConfig = {
     extra-substituters = [
-      "https://install.determinate.systems"
       "https://cache.garnix.io"
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
@@ -14,8 +13,6 @@
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "determinate-systems.cachix.org-1:8hfO/4KM4BUMONABo3NyuTsIB9YLUo5aIwGPg2A6Zs4="
-      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "r3dlust.cachix.org-1:/R3S8pW/nr7kOBJKcGPsZ0zCepvldTUEgbrqa4O3cW0="
     ];
 
