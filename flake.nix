1c3286834aeeb538ce0adc20d956618cded7d730
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-15 19:57:40 -0300

    feat(nix-src): go back to real input (todo cache this)

--- flake.nix
+++ flake.nix
@@ -7,12 +7,15 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://r3dlust.cachix.org"
+      "https://install.determinate.systems"
     ];
 
     extra-trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "determinate-systems.cachix.org-1:8hfO/4KM4BUMONABo3NyuTsIB9YLUo5aIwGPg2A6Zs4="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
       "r3dlust.cachix.org-1:/R3S8pW/nr7kOBJKcGPsZ0zCepvldTUEgbrqa4O3cW0="
     ];
 
@@ -73,12 +76,7 @@
       flake = false;
     };
 
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix.url = "github:GustavoWidman/nix-src-autobuild";
+    nix.url = "https://flakehub.com/f/DeterminateSystems/nix-src/*";
     zed.url = "github:GustavoWidman/zed-autobuild";
   };
 
