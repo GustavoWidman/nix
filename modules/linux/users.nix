8685bfbcf1b5361a3dbaed5efd45e20c6940ef82
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-17 12:46:15 -0300

    revert "feat(shell/linux): patch shell for zed remote"
    This reverts commit e2465c521a588bd71f3c6b306dcd24ef4d8eb423.
    
    nushell has been pached on ssh remotes thanks to
    zed-industries/zed#40084. yay!

--- modules/linux/users.nix
+++ modules/linux/users.nix
@@ -4,35 +4,20 @@
   keys,
   ...
 }:
-let
-  nu-smart = pkgs.writeShellScriptBin "nu-smart" ''
-    if [ $# -gt 0 ]; then
-      if [ "$1" = "-c" ]; then
-        shift
-        exec ${pkgs.bash}/bin/sh -c "$*"
-      else
-        exec ${pkgs.bash}/bin/sh -c "$*"
-      fi
-    else
-      exec ${pkgs.nushell}/bin/nu -l
-    fi
-  '';
-in
+
 {
   users.mutableUsers = false;
-
-  environment.shells = [ "${nu-smart}/bin/nu-smart" ];
 
   users.users = {
     root = {
       hashedPasswordFile = config.secrets.password.path;
       openssh.authorizedKeys.keys = keys.admins;
-      shell = "${nu-smart}/bin/nu-smart";
+      shell = pkgs.nushell;
     };
 
     r3dlust = {
       hashedPasswordFile = config.secrets.password.path;
-      shell = "${nu-smart}/bin/nu-smart";
+      shell = pkgs.nushell;
       name = "r3dlust";
       home = "/home/r3dlust";
       isMainUser = true;
