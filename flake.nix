59c0308c490ac66122efe8a48bed9416705e6855
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-28 12:36:41 -0300

    feat(macos): init rift
    currently not using, but i pretend to switch
    to it once a few annoying bugs are fixed. very
    promising, but also very immature

--- flake.nix
+++ flake.nix
@@ -76,6 +76,16 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    crane.url = "github:ipetkov/crane";
+    rift = {
+      url = "github:acsandmann/rift";
+      flake = false;
+    };
+
     zed.url = "github:GustavoWidman/zed-autobuild";
   };
 
