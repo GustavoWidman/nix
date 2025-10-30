5afa675c3eabe1f509307cd6a5940273b4b2732f
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-10-30 00:10:10 -0300

    chore: remove rift, store for later...

--- flake.nix
+++ flake.nix
@@ -76,16 +76,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane.url = "github:ipetkov/crane";
-    rift = {
-      url = "github:acsandmann/rift";
-      flake = false;
-    };
-
     zed.url = "github:GustavoWidman/zed-autobuild";
   };
 
