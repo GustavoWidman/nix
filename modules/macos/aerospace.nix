bf10a8398c412375574c8a926d45905219a5b80d
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-11-25 12:56:04 -0300

    feat(macos/aerospace): remove alt-tab patch/hack
    this was fixed as of aerospace v0.20.0-Beta

--- modules/macos/aerospace.nix
+++ modules/macos/aerospace.nix
@@ -33,51 +33,6 @@ let
     };
     run = [ "layout floating" ];
   };
-
-  swapWorkspaceScript = pkgs.writeShellScript "swap-workspace-script" ''
-    AEROSPACE="${pkgs.aerospace}/bin/aerospace"
-
-    focus_window() {
-      local direction=$1
-      local window_ids=($($AEROSPACE list-windows --workspace focused --format '%{window-id}'))
-      local window_count=${"$"}{#window_ids[@]}
-
-      if [[ ${"$"}window_count -le 1 ]]; then
-        exit 1
-      fi
-
-      local current_id=$($AEROSPACE list-windows --focused --format '%{window-id}')
-
-      local current_index
-      for i in "${"$"}{!window_ids[@]}"; do
-        if [[ "${"$"}{window_ids[$i]}" == "$current_id" ]]; then
-          current_index=$i
-          break
-        fi
-      done
-
-      local target_index
-      case $direction in
-        left|up)
-          ((target_index = (current_index - 1 + window_count) % window_count))
-          ;;
-        right|down)
-          ((target_index = (current_index + 1) % window_count))
-          ;;
-        *)
-          exit 1
-          ;;
-      esac
-
-      $AEROSPACE focus --window-id "${"$"}{window_ids[$target_index]}"
-    }
-
-    if [[ $# -eq 1 ]]; then
-      focus_window "$1"
-    else
-      exit 1
-    fi
-  '';
 in
 {
   services.aerospace = enabled {
@@ -119,8 +74,8 @@ in
         alt-shift-k = "move up";
         alt-shift-l = "move right";
 
-        alt-tab = "exec-and-forget ${pkgs.aerospace}/bin/aerospace focus --boundaries-action wrap-around-the-workspace right || ${swapWorkspaceScript} right";
-        alt-shift-tab = "exec-and-forget ${pkgs.aerospace}/bin/aerospace focus --boundaries-action wrap-around-the-workspace left || ${swapWorkspaceScript} left";
+        alt-tab = "focus --boundaries-action wrap-around-the-workspace right";
+        alt-shift-tab = "focus --boundaries-action wrap-around-the-workspace left";
 
         alt-1 = "workspace 1";
         alt-2 = "workspace 2";
