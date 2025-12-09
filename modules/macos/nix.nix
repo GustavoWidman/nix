cdb56b72a58a5afa6276d68776b77ab88ad85919
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-12-09 16:23:45 +0300

    fix(nix): correct bad config related to external-builders

--- modules/macos/nix.nix
+++ modules/macos/nix.nix
@@ -9,7 +9,7 @@
   # '';
   #
   nix-settings = {
-    experimental-features = [ "external-builders" ];
+    extra-experimental-features = [ "external-builders" ];
     external-builders = "[{\"systems\":[\"aarch64-linux\",\"x86_64-linux\"],\"program\":\"/usr/local/bin/determinate-nixd\",\"args\":[\"builder\"]}]";
   };
 }
