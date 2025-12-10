ad36c1cb04648ca68f058c8b6580b3c4334bdfdb
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2025-12-10 14:08:02 -0300

    feat(hm/pkgs): re-expose hm packages to system

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -1,7 +1,11 @@
+{ config, ... }:
 {
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
     backupFileExtension = "bak";
   };
+
+  # re-expose hm packages to system
+  environment.systemPackages = config.home-manager.users.r3dlust.home.packages;
 }
