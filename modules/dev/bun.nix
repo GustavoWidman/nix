08fb5dbb5a8a4af6694996f4c93e536c1f3dd871
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2026-06-17 16:58:24 -0300

    feat(bun): update to 1.3.14
    bun 1.3.14 is not on nixpkgs yet and i want it!

--- modules/dev/bun.nix
+++ modules/dev/bun.nix
@@ -8,7 +8,33 @@
     {
       programs.bun = {
         enable = true;
-        package = pkgs.bun;
+        package = pkgs.bun.overrideAttrs (
+          finalAttrs: previousAttrs: {
+            version = "1.3.14";
+            src = finalAttrs.passthru.sources.${pkgs.stdenvNoCC.hostPlatform.system};
+
+            passthru = previousAttrs.passthru // {
+              sources = {
+                "aarch64-darwin" = pkgs.fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-darwin-aarch64.zip";
+                  hash = "sha256-2LliIYKK1vl6x6wKt+lYcjQa92MAHogD6CZ2UsJlJiA=";
+                };
+                "aarch64-linux" = pkgs.fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-linux-aarch64.zip";
+                  hash = "sha256-on/7Y6gxA3WDbg1vZorhf6jY0YuIw3yCHGUzGXOhmjs=";
+                };
+                "x86_64-darwin" = pkgs.fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-darwin-x64-baseline.zip";
+                  hash = "sha256-PjWtb1OXGpg0v55nhuKt9ytfGSHMmpxf3gc9KXKUQHY=";
+                };
+                "x86_64-linux" = pkgs.fetchurl {
+                  url = "https://github.com/oven-sh/bun/releases/download/bun-v${finalAttrs.version}/bun-linux-x64.zip";
+                  hash = "sha256-lR7iruhV8IWVruxiJSJqKY0/6oOj3NZGXAnLzN9+hI8=";
+                };
+              };
+            };
+          }
+        );
       };
     }
   ];
