28935f2382fed8860abc13c8b6d45607439230d6
GustavoWidman (laptop-mac) <admin@r3dlust.com>
2026-09-18 11:47:51 -0300

    feat(dev/zed): auto-open markdown files on preview

--- modules/dev/zed.nix
+++ modules/dev/zed.nix
@@ -275,6 +275,9 @@ in
             };
           };
           cursor_animation.enabled = true;
+          markdown_preview = {
+            open_markdown_files_in_preview = true;
+          };
         };
         userKeymaps = [
           {
